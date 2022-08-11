9a7eb3f716f8b64be3d2732acad33f463298afd6
Gao Xiang <hughgao01@gmail.com>
2022-08-11 19:26:34 +0800

    finish linear search

--- src/linear_search/src/main.rs
+++ src/linear_search/src/main.rs
@@ -6,13 +6,45 @@ fn main() {
     let arr1 = ["ask", "me", "what", "is"];
     assert_eq!(3, linear_search(&arr1, &"is"));
     assert_eq!(-1, linear_search(&arr1, &"test"));
+
+    let arr2 = [
+        Test {
+            test1: String::from("test"),
+            test2: 2,
+            test3: 0.0,
+        },
+        Test {
+            test1: String::from("test1"),
+            test2: 3,
+            test3: 1.0,
+        },
+    ];
+    assert_eq!(1, linear_search(&arr2, &Test {
+        test1: String::from("test1"),
+        test2: 3,
+        test3: 1.0,
+    }));
+    assert_eq!(-1, linear_search(&arr2, &Test {
+        test1: String::from("test1"),
+        test2: 3,
+        test3: 1.001,
+    }));
 }
 
-#[derive(PartialEq)]
 struct Test {
     test1: String,
-    test2: str,
-    test3: double
+    test2: i32,
+    test3: f64,
+}
+
+impl Eq for Test{
+
+}
+
+impl PartialEq for Test {
+    fn eq(&self, other: &Self) -> bool {
+        self.test1 == other.test1 && self.test2 == other.test2 && f64::abs(self.test3 - other.test3) < 1e-5
+    }
 }
 
 fn linear_search<T: Eq>(elements: &[T], element: &T) -> isize {
